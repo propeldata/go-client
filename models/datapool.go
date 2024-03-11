24573a010345d76d3bfc3e41fb1c110ad7a333b5
Margarita Gómez <margarita@propeldata.com>
2024-03-11 09:04:41 -0500

    chore: query table settings (#10)

--- models/datapool.go
+++ models/datapool.go
@@ -8,10 +8,18 @@ type UniqueId struct {
 	ColumnName string `json:"columnName"`
 }
 
+type DataPoolColumn struct {
+	ColumnName string `json:"columnName"`
+	Type       string `json:"type"`
+	IsNullable bool   `json:"isNullable"`
+}
+
 type DataPool struct {
-	ID        string    `json:"id"`
-	Timestamp Timestamp `json:"timestamp"`
-	UniqueID  UniqueId  `json:"uniqueId"`
+	ID             string           `json:"id"`
+	Timestamp      Timestamp        `json:"timestamp"`
+	UniqueID       UniqueId         `json:"uniqueId"`
+	TableSettings  *TableSettings   `json:"tableSettings"`
+	OrderByColumns []DataPoolColumn `json:"orderByColumns,omitempty"`
 }
 
 type FetchDataPoolByName struct {
